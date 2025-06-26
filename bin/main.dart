a27c6562fb3641a4aca36977d88653104f221356
Lucas_397 <lu.machado.m397@gmail.com>
2025-06-26 14:16:37 -0300

    api/functions

--- bin/main.dart
+++ bin/main.dart
@@ -1,19 +1,45 @@
 import 'package:http/http.dart';
+import 'dart:convert';
 
 void main() {
   // print("Olá, mundo!");
-  requestData();
+  requestDataAsync();
+  sendDataAsync();
 }
+  String url =
+  "https://gist.githubusercontent.com/Lucas-397/37296fe80cb4d2043c1cedbc1247f91f/raw/077d0f06aea5eac18c41339e9f7a82492ea46fac/accounts.json";
 
 requestData() {
-  String url =
-      "https://gist.githubusercontent.com/ricarthlima/a0eb198cb7a70696c4031e7e577de0cd/raw/356ce2c39dfd58d3d2e948d1ad87ea828544f1db/accounts.json";
   Future<Response> futureResponse = get(Uri.parse(url));
   print(futureResponse);
   futureResponse.then(
     (Response response) {
-      print(response);
-      print(response.body);
+      List<dynamic> accountsList = json.decode(response.body);
+      Map<String, dynamic> carlaAccount = accountsList.firstWhere(
+        (element) => element["name"] == "Carla"
+      );
+
+      print(carlaAccount);
     },
   );
+
+}
+
+Future<List<dynamic>> requestDataAsync() async{
+  Response resposta = await get(Uri.parse(url));
+  return json.decode(resposta.body);
+}
+
+Future  sendDataAsync() async{
+  List<dynamic> contas = await requestDataAsync();
+  contas.add({
+    "id": "Id011",
+    "name": "Lucas",
+    "lastName": "Machado",
+    "balance": 1000.0
+  });
+  String content = json.encode(contas);
+
+  Response reposta = await post(Uri.parse(url), body: content);
+  print(reposta.statusCode);
 }
